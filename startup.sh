57ac77984cd17dc8782a1ca734475f98177159dc
javiplav <javier.plavnick@run.ai>
2024-02-14 23:12:14 +0200

    Delete startup.sh

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Downloading CIFAR10 dataset"
-wget -nc https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar zxf cifar-10-python.tar.gz
-rm cifar-10-python.tar.gz
-python main.py
