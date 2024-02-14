ea20077ce7136ae6485620639e80b170f9bca35a
javiplav <javier.plavnick@run.ai>
2024-02-15 00:26:00 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,10 +4,6 @@ apt-get update > /dev/null 2>&1 && apt-get install wget -y > /dev/null 2>&1
 wait $!
 # Next commands using wget will run here
 echo "wget installation complete!"
-if ! command -v wget &> /dev/null; then
-  echo "Error: wget not found. Please install it."
-  exit 1
-fi
 wget -nc https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 wait
 echo "Download complete!"
