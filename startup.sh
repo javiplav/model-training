3b77b45a8bdbb3e43f8c1d1ab78a475e00b99468
javiplav <javier.plavnick@run.ai>
2024-02-15 00:17:00 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 echo "Downloading CIFAR10 dataset"
+sudo apt-get install wget
+wait
+if ! command -v wget &> /dev/null; then
+  echo "Error: wget not found. Please install it."
+  exit 1
+fi
 wget -nc https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 wait
 echo "Download complete!"
 tar zxf cifar-10-python.tar.gz
-rm cifar-10-python.tar.gz
+rm cifar-10-python.tar.gzc
 python main.py
