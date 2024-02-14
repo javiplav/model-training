e4a1591176ba997888c18bbe2a1f1baf679cc4ce
javiplav <javier.plavnick@run.ai>
2024-02-15 00:45:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def parse_args():
 
 
 def create_train_loader(args, **kwargs):
-    train_dataset = datasets.CIFAR10('/workspace/model-training.git', train=True, download=False,
+    train_dataset = datasets.CIFAR10('/workspace/model-training.git', train=True, download=True,
         transform=transforms.Compose([
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
@@ -48,7 +48,7 @@ def create_train_loader(args, **kwargs):
 
 
 def create_test_loader(args, **kwargs):
-    test_dataset = datasets.CIFAR10('/workspace/model-training.git', train=False, download=False,
+    test_dataset = datasets.CIFAR10('/workspace/model-training.git', train=False, download=True,
         transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
