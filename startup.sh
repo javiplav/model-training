d05e9bd12619f73c145e0fc6b36a17874d832645
javiplav <javier.plavnick@run.ai>
2024-02-15 11:32:24 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-echo "Usage: $0 $1 $2"
+for arg in "$@"; do
+  case "$arg" in
+    --repository_url=*)
+      repository_url="${arg#*=}"
+      ;;
+    --commit_id=*)
+      commit_id="${arg#*=}"
+      ;;
+  esac
+done
+echo "Using $repository_url and $commit_id"
 echo "Downloading CIFAR10 dataset"
 apt-get update > /dev/null 2>&1 && apt-get install wget -y > /dev/null 2>&1 && apt-get install git -y > /dev/null 2>&1
 wait $!
@@ -12,8 +22,6 @@ tar zxf cifar-10-python.tar.gz
 wait $!
 rm cifar-10-python.tar.gz
 wait $!
-repository_url="$1"
-commit_id="$2"
 git clone --branch "$commit_id" "$repository_url"
 wait $!
 echo "Successfully cloned repository to ./$repository_name at commit $commit_id"
